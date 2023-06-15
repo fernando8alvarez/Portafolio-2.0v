254e6b083ed030f758acfeecfe876f3590826cd2
Luis Fernando Alvarez <fernando_0694@hotmail.com>
2023-06-14 22:36:03 -0400

    arreglos-2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -127,8 +127,8 @@ export default function Navbar({ currentSection }) {
                                     <a
                                         href={`#${el.id}`}
                                         onClick={() => setmenu(false)}
-                                        className={`${(el.id === currentSection ? "text-[#c5ff1e] text-base sm:text-lg" : "text-[#e5e3df]")}
-                                            font-Montserrat hover:text-[#c5ff1e] text-sm sm:text-base hover:text-base hover:sm:text-lg
+                                        className={`${(el.id === currentSection ? "text-[#c5ff1e] text-base sm:text-lg" : "text-[#e5e3df] text-sm sm:text-base")}
+                                            font-Montserrat hover:text-[#c5ff1e] hover:text-base hover:sm:text-lg
                                             bg-transparent
                                             pl-3 pr-4 md:p-0  
                                             transform duration-500 no-underline`}
