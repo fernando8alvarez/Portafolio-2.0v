69d2c7a730f02f19a758844617175ec96b44e82e
Luis Fernando Alvarez <fernando_0694@hotmail.com>
2023-07-16 19:09:12 -0400

    refactor: animacion logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -73,9 +73,9 @@ export default function Navbar({ currentSection }) {
         <Nav bg={isScrolled ? "bg-[#000000ae]" : "bg-transparent"}>
             <animated.div style={animatedNavbar} className=" px-10 sm:px-20 md:px-10 py-2">
                 <Container1>
-                    <a href="#top" className="flex items-center">
+                    <Link className="flex items-center cursor-pointer" to="section-1" spy={true} smooth={true} offset={-60} duration={600}>
                         <img src={logo} className="h-12 sm:h-16" alt="Logo" />
-                    </a>
+                    </Link>
                     <Menu
                         data-collapse-toggle="navbar-default"
                         type="button"
@@ -104,9 +104,9 @@ export default function Navbar({ currentSection }) {
                                     return (
                                         <div key={idGenerate()}
                                             className={`transform duration-500 hover:-translate-y-1 ${(el.id === currentSection && "-translate-y-1")}`}>
-                                            <Link 
-                                            to={`${el.id}`} spy={true} smooth={true} offset={-60} duration={600}
-                                            className={`${(el.id === currentSection ? "text-[#c5ff1e]" : "text-[#e5e3df]")}
+                                            <Link
+                                                to={`${el.id}`} spy={true} smooth={true} offset={-60} duration={600}
+                                                className={`${(el.id === currentSection ? "text-[#c5ff1e]" : "text-[#e5e3df]")}
                                             font-Montserrat hover:text-[#c5ff1e] text-sm xl:text-base
                                             cursor-pointer
                                             bg-transparent
@@ -128,7 +128,7 @@ export default function Navbar({ currentSection }) {
                             return (
                                 <div key={idGenerate()} className="cursor-auto h-5 transform duration-500">
                                     <Link
-                                        
+
                                         onClick={() => setmenu(false)}
                                         to={`${el.id}`} spy={true} smooth={true} offset={-60} duration={600}
                                         className={`${(el.id === currentSection ? "text-[#c5ff1e] text-base sm:text-lg" : "text-[#e5e3df] text-sm sm:text-base")}
