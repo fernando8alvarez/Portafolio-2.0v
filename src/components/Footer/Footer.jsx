f941ba351c35fedbd9e937388c67fe09f42112c7
Fernando Alvarez <fernando_0694@hotmail.com>
2023-10-03 18:02:58 -0400

    refactor: estilos Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,44 +3,72 @@ import linkedin from '../../imgs/linkedin.png';
 import github from '../../imgs/github.png';
 import email from './imgs/mail.png';
 import ws from './imgs/whatsapp.png';
+import {
+    Foter,
+    Container,
+    Logo,
+    SubContainer,
+    Tittle,
+    Icons,
+    Link,
+    Copyright
+} from './styles.js';
 
-
+const red = {
+    linkedin: {
+        name: "Linkedi",
+        link: 'https://www.linkedin.com/in/luis-fernando-a-3a5b7b151/'
+    },
+    github: {
+        name: "GitHub",
+        link: 'https://github.com/fernando8alvarez'
+    }
+}
 
 export default function Footer() {
+
     return (
-        <div className='bg-[#040303] w-full flex flex-col justify-center items-center gap-4 pt-5 sm:pt-8 xl:pt-10 pb-1'>
-            <div className='max-w-screen-lg flex gap-5 sm:gap-10 md:gap-16 lg:gap-24 xl:gap-28'>
-                <img src={logo} alt="img not fount" className='w-[150px] sm:w-[200px] md:w-[250px] lg:w-[350px] xl:w-[450px]' />
-                <div className='flex gap-5 text-[#e5e3df] font-Montserrat text-[10px] sm:text-xs md:text-sm lg:p-10'>
+        <Foter>
+            <Container>
+                <Logo src={logo} alt="img not fount" />
+                < SubContainer >
                     <div className='flex flex-col gap-2 lg:gap-5'>
-                        <p className='font-semibold lg:text-base uppercase m-0'>Redes</p>
+                        <Tittle>Redes</Tittle>
                         <div className='flex gap-3 items-center m-0'>
-                            <img src={linkedin} alt="Img not found" className='w-4 h-4 lg:w-5 lg:h-5' />
-                            <a href='https://www.linkedin.com/in/luis-fernando-a-3a5b7b151/' target="_blank" className='m-0 no-underline text-[#e5e3df] hover:text-[#c5ff1e] transform duration-500'>LinkedIn</a>
+                            <Icons src={linkedin} alt="Img not found" />
+                            <Link
+                                href={red.linkedin.link}
+                                target="_blank">
+                                {red.linkedin.name}
+                            </Link>
                         </div>
                         <div className='flex gap-3 items-center '>
-                            <img src={github} alt="Img not found" className='w-4 h-4 lg:w-5 lg:h-5' />
-                            <a href='https://github.com/fernando8alvarez' target="_blank" className='m-0 no-underline text-[#e5e3df] hover:text-[#c5ff1e] transform duration-500'>GitHub</a>
+                            <Icons src={github} alt="Img not found" />
+                            <Link
+                                href={red.github.link}
+                                target="_blank">
+                                {red.github.name}
+                            </Link>
                         </div>
                     </div>
                     <div className='flex flex-col gap-2 lg:gap-5'>
-                        <p className='font-semibold lg:text-base uppercase m-0'>Contactos</p>
+                        <Tittle>Contactos</Tittle>
                         <div className='flex gap-3 items-center m-0'>
-                            <img src={ws} alt="Img not found" className='w-4 h-4 lg:w-5 lg:h-5' />
-                            <a target="_blank" className='m-0 no-underline text-[#e5e3df] hover:text-[#c5ff1e] transform duration-500'>Whatsapp</a>
+                            <Icons src={ws} alt="Img not found" />
+                            <Link target="_blank">Whatsapp</Link>
                         </div>
                         <div className='flex gap-3 items-center '>
-                            <img src={email} alt="Img not found" className='w-4 h-4 lg:w-5 lg:h-5' />
-                            <a target="_blank" className='m-0 no-underline text-[#e5e3df] hover:text-[#c5ff1e] transform duration-500'>Correo</a>
+                            <Icons src={email} alt="Img not found" />
+                            <Link target="_blank">Correo</Link>
                         </div>
                     </div>
-                </div>
-            </div>
+                </SubContainer>
+            </Container>
             <div>
-                <p className='font-Montserrat text-[#e5e3df] text-[10px] sm:text-xs md:text-sm xl:text-lg font-medium'>
+                <Copyright >
                     © Copyright 2023 | Coded by Luis Fernando Alvarez
-                </p>
+                </Copyright>
             </div>
-        </div>
+        </Foter>
     )
 }
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,55 @@
+import tw from "tailwind-styled-components";
+import { animated } from 'react-spring';
+
+const Foter = tw.footer`
+w-full
+bg-[#040303] 
+flex flex-col gap-4
+justify-center items-center  
+pt-5 sm:pt-8 xl:pt-10 pb-1
+`
+
+const Container = tw.div`
+max-w-screen-lg 
+flex gap-5 sm:gap-10 md:gap-16 lg:gap-24 xl:gap-28
+`
+
+const Logo = tw.img` 
+w-[150px] sm:w-[200px] md:w-[250px] lg:w-[350px] xl:w-[450px]
+`
+
+const SubContainer = tw.div`
+flex gap-5 
+text-[#e5e3df] font-Montserrat text-[10px] sm:text-xs md:text-sm 
+lg:p-10
+`
+const Tittle = tw.p` 
+font-semibold lg:text-base 
+uppercase m-0
+`
+
+const Icons = tw.img` 
+w-4 h-4 lg:w-5 lg:h-5
+`
+
+const Link = tw.a`
+text-[#e5e3df] hover:text-[#c5ff1e] 
+m-0 no-underline 
+transform duration-500
+`
+const Copyright = tw.p`
+font-Montserrat text-[#e5e3df] font-medium
+text-[10px] sm:text-xs md:text-sm xl:text-lg 
+`
+
+
+export {
+    Foter,
+    Container,
+    Logo,
+    SubContainer,
+    Tittle,
+    Icons,
+    Link,
+    Copyright
+}
